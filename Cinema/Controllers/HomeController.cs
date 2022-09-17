78d2a64bfbc0fe988d785590f4f9fba34d0b3470
Nikita Kishinka <nikita.kishinka@devs-universe.com>
2022-09-17 13:07:38 +0300

    V2

--- Cinema/Controllers/HomeController.cs
+++ Cinema/Controllers/HomeController.cs
@@ -6,21 +6,28 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Cinema.Dal.Dbo;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace Cinema.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly UserManager<UsersDbo> _userManager;
+
+        public HomeController(UserManager<UsersDbo> userManager)
         {
-            _logger = logger;
+            _userManager = userManager;
         }
 
+        [Authorize(Roles = "User")]
         public IActionResult Index()
         {
-            return View();
+            string userName = _userManager.GetUserName(User);
+            return View("Index", userName);
         }
 
         public IActionResult Privacy()
--- Cinema/Controllers/MovieController.cs
+++ Cinema/Controllers/MovieController.cs
@@ -35,11 +35,6 @@ namespace Cinema.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
